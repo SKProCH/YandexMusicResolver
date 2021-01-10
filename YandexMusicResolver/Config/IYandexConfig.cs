aac1bdb1ffaadb78e5d96945b57664aa864c5bce
SKProCH <skproch@yandex.ru>
2021-01-10 20:09:35 +0300

    Add IYandexConfig

---
+++ YandexMusicResolver/Config/IYandexConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace YandexMusicResolver.Config {
+    public interface IYandexConfig {
+        void Load();
+        void Save();
+        
+        string? YandexLogin { get; set; }
+        string? YandexPassword { get; set; }
+        
+        string? YandexToken { get; set; }
+        
+        IWebProxy? YandexProxy { get; set; }
+    }
+}
