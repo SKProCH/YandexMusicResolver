e55edefe3bb0ad147931bbc269425e3d7f898c87
SKProCH <skproch@yandex.ru>
2021-03-07 17:56:28 +0300

    Add login failure test

---
+++ YandexMusicResolver.Tests/YandexMusicAuthTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Security.Authentication;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace YandexMusicResolver.Tests {
+    public class YandexMusicAuthTest : YandexTestBase{
+        private readonly ITestOutputHelper _output;
+        public YandexMusicAuthTest(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        [Fact]
+        public void AuthFailure() {
+            Assert.ThrowsAsync<InvalidCredentialException>(async () => {
+                await YandexMusicAuth.LoginAsync("Invalid", "Invalid", Config);
+            });
+        }
+    }
+}
