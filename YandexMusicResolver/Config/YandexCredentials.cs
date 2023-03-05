7cd915ad9894eecb4a1f9d0f677dc95717e9269b
SKProCH <skproch@yandex.ru>
2023-03-05 19:06:49 +0300

    Add YandexCredentials class for YandexCredentialsProvider

---
+++ YandexMusicResolver/Config/YandexCredentials.cs
@@ -0,0 +1,26 @@
+﻿namespace YandexMusicResolver.Config;
+
+/// <summary>
+/// Class for storing credentials for <see cref="YandexCredentialsProvider"/>
+/// </summary>
+public class YandexCredentials {
+    /// <summary>
+    /// Login for authorizing in Yandex
+    /// </summary>
+    public string? Login { get; set; }
+
+    /// <summary>
+    /// Password for authorizing in Yandex
+    /// </summary>
+    public string? Password { get; set; }
+
+    /// <summary>
+    /// Token for authorizing in Yandex
+    /// </summary>
+    public string? Token { get; set; }
+
+    /// <summary>
+    /// Is anonymous access allowed
+    /// </summary>
+    public bool AllowAnonymous { get; set; }
+}
