e4de1f1355b52291e869c0d9a5a2fbdb0fd88d85
SKProCH <skproch@yandex.ru>
2025-06-22 18:10:49 +0300

    Add YandexAnonymousCredentialProvider to faster access without login

---
+++ YandexMusicResolver/Config/YandexAnonymousCredentialProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace YandexMusicResolver.Config;
+
+/// <inheritdoc />
+public class YandexAnonymousCredentialProvider : IYandexCredentialsProvider {
+    /// <summary>
+    /// Default ctor
+    /// </summary>
+    protected YandexAnonymousCredentialProvider() { }
+    
+    /// <summary>
+    /// Singleton instance
+    /// </summary>
+    public static YandexAnonymousCredentialProvider Instance { get; } = new();
+
+    /// <inheritdoc />
+    public Task<string?> GetTokenAsync() {
+        return Task.FromResult<string?>(null);
+    }
+
+    /// <inheritdoc />
+    public Task<string?> ValidateOrRetrieveTokenAsync() {
+        return Task.FromResult<string?>(null);
+    }
+
+    /// <inheritdoc />
+    public event EventHandler<YandexTokenChangedEventArgs>? TokenChanged;
+}
